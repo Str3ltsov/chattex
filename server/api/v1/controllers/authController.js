ce11a663c6b0b3f2a44199b584d2d2a81639e7f8
Str3ltsov <andtaress@live.com>
2023-10-23 01:08:36 +0300

    Logout request and response functionality

--- server/api/v1/controllers/authController.js
+++ server/api/v1/controllers/authController.js
@@ -70,8 +70,13 @@ export const resetPassword = async (request, response) => {
 }
 
 export const logout = async (request, response) => {
+    response.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+
     response.status(200).json({
-        'status': 'OK',
-        'message': 'Success.'
+        'status': 'Success',
+        'message': 'Successfully logged out.'
     })
 }
