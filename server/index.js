df4b870f04c6f06047f8f54ff7ac501c9fa5797d
Str3ltsov <andtaress@live.com>
2023-11-23 02:59:16 +0200

    Fixed cors error when enabling credentials

--- server/index.js
+++ server/index.js
@@ -25,7 +25,14 @@ const app = express()
 // Configs
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 
 // Routes
 app.use('/api/v1/auth', userRoutes)
