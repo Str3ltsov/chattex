d76e9df28df34f5306fc39b3c5dbe3d723e98423
Str3ltsov <andtaress@live.com>
2023-12-02 23:21:48 +0200

    Provided tags for every auth api endpoint

--- client/src/redux/slices/authApiSlice.js
+++ client/src/redux/slices/authApiSlice.js
@@ -13,28 +13,32 @@ export const authApiSlice = apiSlice.injectEndpoints({
                 url: `${AUTH_LOGIN_URL}`,
                 method: 'POST',
                 body: data
-            })
+            }),
+            providesTags: ['User']
         }),
         register: builder.mutation({
             query: data => ({
                 url: `${AUTH_REGISTER_URL}`,
                 method: 'POST',
                 body: data
-            })
+            }),
+            providesTags: ['User']
         }),
         forgotPassword: builder.mutation({
             query: data => ({
                 url: `${AUTH_FORGOT_PASSWORD_URL}`,
                 method: 'POST',
                 body: data
-            })
+            }),
+            providesTags: ['User']
         }),
         resetPassword: builder.mutation({
             query: ({ resetPasswordToken, password, confirmPassword }) => ({
                 url: `${AUTH_RESET_PASSWORD_URL}/${resetPasswordToken}`,
                 method: 'PATCH',
                 body: { password, confirmPassword }
-            })
+            }),
+            providesTags: ['User']
         })
     })
 })
